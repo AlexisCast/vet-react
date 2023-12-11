b397afecbecb297587ca1ac1b2f6df1eaec16aa0
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-12-11 05:45:31 -0600

    update Products & add fetchProducts

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 import { mockProducts } from "../mock/products";
 
+import ProductList from "../components/ProductList/ProductList";
+
 const Products = () => {
-	const products = mockProducts;
+	const [isLoading, setIsLoading] = useState(false);
+	const [fetchedProducts, setFetchedProducts] = useState();
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		async function fetchProducts() {
+			setIsLoading(true);
+			const response = await fetch("http://localhost:8080/api/products");
+
+			if (!response.ok) {
+				setError("Fetching products failed.");
+			} else {
+				const resData = await response.json();
+				console.log(await resData);
+				console.log(await resData.products);
+				console.table(await resData.products);
+
+				setFetchedProducts(resData.products);
+			}
+			setIsLoading(false);
+		}
+
+		fetchProducts();
+	}, []);
 
 	return (
 		<div>
@@ -10,14 +37,13 @@ const Products = () => {
 			<p>
 				Go to <Link to="/">HomePage</Link>
 			</p>
-			<ul>
-				{products.map((prod) => (
-					<li key={prod.id}>
-						<Link to={`/products/${prod.id}`}>{prod.title}</Link>{' - '}
-						<Link to={`/products/${prod.id}/edit`}>Edit</Link>
-					</li>
-				))}
-			</ul>
+			<div style={{ textAlign: "center" }}>
+				{isLoading && <p>Loading...</p>}
+				{error && <p>{error}</p>}
+			</div>
+			{!isLoading && fetchedProducts && (
+				<ProductList products={fetchedProducts} />
+			)}
 		</div>
 	);
 };
