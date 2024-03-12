4a10e21d7374ef92897b58c7d26d652b7c6365cb
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-03-12 17:30:34 -0600

    update Table component and styles

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,64 +1,80 @@
 import classNames from "classnames";
 import styles from "./Table.module.css";
 
-export const TableContainer = ({ children, className }) => {
+export const TableContainer = ({ children, className, ...props }) => {
 	return (
-		<div className={classNames(styles.table__container, className)}>
+		<div
+			className={classNames(styles.table__container, className)}
+			{...props}
+		>
 			{children}
 		</div>
 	);
 };
 
-export const Table = ({ children, className }) => {
+export const Table = ({ children, className, ...props }) => {
 	return (
-		<table className={classNames(styles.table, className)}>
+		<table className={classNames(styles.table, className)} {...props}>
 			{children}
 		</table>
 	);
 };
 
-export const TableHeader = ({ children, className }) => {
+export const TableHeader = ({ children, className, ...props }) => {
 	return (
-		<thead className={classNames(styles.tableHeader, className)}>
+		<thead className={classNames(styles.tableHeader, className)} {...props}>
 			{children}
 		</thead>
 	);
 };
 
-export const TableHeaderCell = ({ children, width, className }) => {
+export const TableHeaderCell = ({ children, width, className, ...props }) => {
 	return (
 		<th
 			className={classNames(styles.tableHeaderCell, className, {
-				[styles.width_80]: width === 80,
-				[styles.width_100]: width === 100,
-				[styles.width_150]: width === 150,
-				[styles.width_250]: width === 250,
-				[styles.width_350]: width === 350,
+				[styles.TableHeaderCell__width_80]: width === 80,
+				[styles.TableHeaderCell__width_100]: width === 100,
+				[styles.TableHeaderCell__width_150]: width === 150,
+				[styles.TableHeaderCell__width_250]: width === 250,
+				[styles.TableHeaderCell__width_350]: width === 350,
 			})}
+			{...props}
 		>
 			{children}
 		</th>
 	);
 };
 
-export const TableRow = ({ children, ...props }) => {
-	return <tr {...props}>{children}</tr>;
+export const TableBody = ({ children, className, ...props }) => {
+	return (
+		<tbody className={classNames(styles.tableBody, className)} {...props}>
+			{children}
+		</tbody>
+	);
 };
 
-export const TableBody = ({ children }) => {
-	return <tbody>{children}</tbody>;
+export const TableRow = ({ children, className, ...props }) => {
+	return (
+		<tr className={classNames(styles.tableRow, className)} {...props}>
+			{children}
+		</tr>
+	);
 };
 
-export const TableDataCell = ({ children, ...props }) => {
-	return <td {...props}>{children}</td>;
+export const TableDataCell = ({ children, className, ...props }) => {
+	return (
+		<td className={classNames(styles.tableDataCell, className)} {...props}>
+			{children}
+		</td>
+	);
 };
 
 export default {
-	TableContainer,
 	Table,
-	TableHeader,
-	TableRow,
 	TableBody,
+	TableContainer,
 	TableDataCell,
+	TableHeader,
 	TableHeaderCell,
+	TableRow,
 };
